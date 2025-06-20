7466aa7c45926443f186d3b62a2e19228d4473a3
godfreyM04 <gomark026@gmail.com>
2025-06-20 14:22:07 +0300

    Finished Assignment

--- selection_sort/selection_sort.py
+++ selection_sort/selection_sort.py
@@ -1,3 +1,13 @@
 def selection_sort(arr):
-    # TODO: Implement selection sort
+    n = len(arr)
+    for i in range(n):
+        min_index = i
+        for j in range(i + 1, n):
+            if arr[j] < arr[min_index]:
+                min_index = j
+        arr[i], arr[min_index] = arr[min_index], arr[i]
     pass
+
+my_list = [6,2,44,5,59,3,1,3]
+selection_sort(my_list)
+print(my_list)
